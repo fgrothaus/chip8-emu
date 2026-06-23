ab95217d6f9f807678c980d6bda54a748d3d171b
fgrothaus <felixgrothaus1@gmail.com>
2026-06-23 21:01:47 +0200

    finished emulator

--- Makefile
+++ Makefile
@@ -1,8 +1,26 @@
+# 1. Betriebssystem automatisch erkennen
+ifeq ($(OS),Windows_NT)
+    # --- WINDOWS SETUP ---
+    TARGET = chip8.exe
+    RM = del /Q /F
+    # Das Windows-Compiler-Monster mit allen System-Bibliotheken
+    LIBS = -lmingw32 -lSDL2main -lSDL2 -mwindows -lwinmm -limm32 -lversion -lole32 -loleaut32 -luuid -lgdi32 -lsetupapi -lshlwapi
+    CFLAGS = -Iinclude -Llib
+else
+    # --- LINUX SETUP ---
+    TARGET = chip8
+    RM = rm -f
+    # Unter Linux reicht das einfache lSDL2-Flag
+    LIBS = -lSDL2
+    CFLAGS = 
+endif
+
+# 2. Die eigentlichen Build-Befehle (bleiben für beide Systeme gleich!)
 all:
-	gcc src/main.c src/cpu.c -o chip8
+	gcc src/main.c src/cpu.c $(CFLAGS) -o $(TARGET) $(LIBS)
 
 run: all
-	./chip8
+	./$(TARGET)
 
 clean:
-	rm -f chip8 a.out
+	$(RM) $(TARGET)
--- src/main.c
+++ src/main.c
@@ -1,27 +1,80 @@
 #include <stdio.h>
-#include <unistd.h> // Für usleep
+#ifdef _WIN32
+    #include <windows.h> // Für Sleep() auf Windows
+#else
+    #include <unistd.h>    // Für usleep() auf Linux
+#endif
+#include <SDL2/SDL.h> // Für Grafiken
 
 #include "cpu.h"
 
-int main() {
+#define SCREEN_SCALE 10
+#define CH8_WIDTH 64
+#define CH8_HEIGHT 32
+
+void debug_render_terminal(Chip8* chip8) {
+    // Terminal aufräumen/nach oben flashen (ANSI Escape Code)
+    printf("\033[H"); 
+
+    printf("\n+----------------------------------------------------------------+\n");
+    for (int y = 0; y < CH8_HEIGHT; y++) {
+        printf("|");
+        for (int x = 0; x < CH8_WIDTH; x++) {
+            if (chip8->display[x + (y * 64)] == 1) {
+                printf("#"); // Leuchtender Pixel
+            } else {
+                printf(" "); // Schwarzer Pixel
+            }
+        }
+        printf("|\n");
+    }
+    printf("+----------------------------------------------------------------+\n");
+}
+
+int main(int argc, char* argv[])
+{
     // run code: make run
     Chip8 chip8;
 
-
     chip8_init(&chip8);
-    printf("delay_timer nach Init: %d\n", chip8.delay_timer); // chip8-> Schreibweise nur bei Pointern
-
     chip8_load_rom(&chip8, "roms/IBM Logo.ch8");
 
     printf("\n--- SPEICHER-KONTROLLE ---\n");
     printf("Byte bei RAM-Adresse 0x200: 0x%02X\n", chip8.memory[0x200]);
     printf("Byte bei RAM-Adresse 0x201: 0x%02X\n", chip8.memory[0x201]);
     
+    // --- SDL2 INITIALISIERUNG ---
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL Fehler: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Fenster erstellen
+    SDL_Window* window = SDL_CreateWindow(
+        "Mein genialer CHIP-8 Emulator",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        CH8_WIDTH * SCREEN_SCALE, CH8_HEIGHT * SCREEN_SCALE,
+        SDL_WINDOW_SHOWN
+    );
+
+    // Renderer (Pinsel) erstellen
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
 
     int timer_accumulator = 0;
+    int running = 1;
+    SDL_Event event;
 
-    while (1)
-    {   
+    while (running)
+    {
+        // 1. SDL2 Events abfragen (z.B. Fenster schließen)
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
         chip8_cycle(&chip8); // Einen Opcode verarbeiten
         timer_accumulator++;
 
@@ -33,22 +86,56 @@ int main() {
             // Eine Iteration einer Loop dauert ca. 2ms => timer_accumulator = 8 * 2ms = 16ms => Ziemlich genau die Zeit pro Frequenz (T) von 60Hz (1000ms / 60 = 16,66ms)
             // Eine Frequenz beschreibt die Häufigkeit pro Sekunde. Deswegen sind T = 1/Frequenz in Hz
             // T = 1s/60Hz = 16,66ms => Wenn delay_timer = 120 ist, wird 120*16,66ms gerechnet => 1999,2ms => ~2s
-            if (chip8.delay_timer > 0)
-            {
-                chip8.delay_timer--;
-            }
-
-            if (chip8.sound_timer > 0)
-            {
-                chip8.sound_timer--;
-                // Beep Ton triggern über Lib
-            }
+            if (chip8.delay_timer > 0) chip8.delay_timer--;
+            if (chip8.sound_timer > 0) chip8.sound_timer--; // Beep Ton triggern über Lib
+            
+            //debug_render_terminal(&chip8);
 
             timer_accumulator = 0;
         }
 
-        usleep(2000); // u in usleep steht für Microseconds (Wimpernschlag - zu vernachlässigen)
+        // 2. GRAFIK-RENDERING: Wir zeichnen den aktuellen Zustand des CHIP-8 Displays
+        // Hintergrund komplett schwarz färben
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Wir setzen die Zeichenfarbe für leuchtende Pixel auf Weiß
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // Wir scannen das flache display-Array (64 * 32 = 2048 Pixel)
+        for (int y = 0; y < CH8_HEIGHT; y++) {
+            for (int x = 0; x < CH8_WIDTH; x++) {
+                // Wir nutzen wieder unsere gewohnte Formel fürs flache Array!
+                if (chip8.display[x + (y * 64)] == 1) {
+                    // Ein Quadrat (Rechteck) für den skalierten Pixel definieren
+                    SDL_Rect pixel_rect;
+                    pixel_rect.x = x * SCREEN_SCALE;
+                    pixel_rect.y = y * SCREEN_SCALE;
+                    pixel_rect.w = SCREEN_SCALE;
+                    pixel_rect.h = SCREEN_SCALE;
+
+                    // Das Quadrat weiß ausfüllen
+                    SDL_RenderFillRect(renderer, &pixel_rect);
+                }
+            }
+        }
+
+        // Den virtuellen Zeichenpuffer auf den echten Monitor bringen
+        SDL_RenderPresent(renderer);
+
+        #ifdef _WIN32
+            Sleep(2); // Windows nutzt Millisekunden (2 ms)
+        #else
+            usleep(2000); // u in usleep steht für Microseconds (Wimpernschlag - zu vernachlässigen)
+        #endif
     }
     
+    printf("SDL2 erfolgreich gestartet!\n");
+
+    // Aufräumen, wenn das Programm beendet wird
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
